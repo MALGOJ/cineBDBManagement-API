70c21dd6b6e32a0dbc28d20e15fa0e09a4d6d447
MALGOJ <manuelgomezjimenez304@gmail.com>
2025-04-05 07:54:26 -0500

    initial commit

---
+++ src/main/java/com/example/cineBDB_managment/CineBdbManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.cineBDB_managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineBdbManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineBdbManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cineBDB_managment/CineBdbManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cineBDB_managment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineBdbManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
